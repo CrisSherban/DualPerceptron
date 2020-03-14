598a1473d1e5b12591adad568dfbb41ff116ff2e
Cristian Tudosie <cris.sherban.t@gmail.com>
2020-03-14 12:37:43 +0100

    Initial commit

---
+++ Perceptron.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+
+class Perceptron:
+
+    def __init__(self, kernel="linear_ker", threshold=250, dim=None, sigma=None):
+        self.threshold = threshold
+        self.sigma = sigma
+        self.dim = dim
+        self.kernel = kernel
+        self.X = None
+        self.y = None
+        self.alpha = None
+        self.R = None
+        self.b = None
+
+    # Predicts a a TestSet and returns the accuracy
+    def predict_set(self, X, y):
+        actual = y
+        predicted = []
+        for i in range(0, len(y)):
+            predicted.append(self.predict(X[i]))
+        return self.accuracy(actual, predicted)
+
+    def predict(self, xi):
+        summation = 0
+
+        for j in range(0, len(self.X[0])):
+            if self.kernel == "rbf_ker":
+                summation += (self.alpha[j] * self.y[j] * self.rbf_ker(self.X[j], xi, self.sigma)) + self.b
+            if self.kernel == "polynomial_ker":
+                summation += (self.alpha[j] * self.y[j] * self.polynomial_ker(self.X[j], xi, self.dim)) + self.b
+            else:
+                summation += (self.alpha[j] * self.y[j] * self.linear_ker(self.X[j], xi)) + self.b
+
+        if summation > 0:
+            activation = 1
+        else:
+            activation = -1
+        return activation
+
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+        self.alpha = np.zeros(len(X[0]))
+        self.b = 0
+
+        norms = []
+
+        for i in range(0, len(X[0])):
+            norm_of_i = np.linalg.norm(X[i])
+            norms.append(norm_of_i)
+
+        self.R = max(norms)
+
+        for k in range(0, self.threshold):
+            for i in range(len(X[0])):
+                if y[i] * self.predict(X[i]) <= 0:
+                    # print(self.alpha)
+                    self.alpha[i] = self.alpha[i] + 1
+                    self.b += y[i] * (self.R ** 2)
+
+    # Calculate accuracy percentage
+    def accuracy(self, actual, predicted):
+        correct = 0
+        for i in range(len(actual)):
+            if actual[i] == predicted[i]:
+                correct += 1
+        return correct / float(len(actual)) * 100.0
+
+    def linear_ker(self, x, z):
+        return np.dot(x, z)
+
+    def polynomial_ker(self, x, z, dim):
+        return (np.dot(x, z)) ** dim
+
+    def rbf_ker(self, x, z, sigma):
+        return np.exp(-(np.linalg.norm(x - z) ** 2) / (2.0 * (sigma ** 2)))
---
+++ Tests.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+import Perceptron as neurone
+
+
+# Convert string column to float
+def str_column_to_float(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+
+# Convert string column to integer
+def str_column_to_int(dataset, column):
+    class_values = [row[column] for row in dataset]
+    unique = set(class_values)
+    lookup = dict()
+    for i, value in enumerate(unique):
+        lookup[value] = i
+    for row in dataset:
+        row[column] = lookup[row[column]]
+    return lookup
+
+
+def setup_dataset(dataset):
+    for i in range(0, 14):
+        str_column_to_int(dataset, i)
+
+    for i in range(1, len(dataset[:, 14])):
+        if dataset[i, 14] == "<=50K":
+            dataset[i, 14] = -1
+        else:
+            dataset[i, 14] = 1
+
+    # 39000 entries for the train the rest 9842 for test
+    split = 42001
+
+    train = (dataset[1:split, :14], dataset[1:split, 14])
+    test = (dataset[split:, :14], dataset[split:, 14])
+
+    return train, test
+
+
+##################################################################
+
+df = pd.read_csv('census_income_dataset.csv', sep=',', header=None)
+dataset = df.to_numpy()
+
+((X_train, y_train), (X_test, y_test)) = setup_dataset(dataset)
+
+clf_linear = neurone.Perceptron(kernel="linear_ker")
+clf_poly = neurone.Perceptron(kernel="polynomial_ker", dim=2)
+clf_rbf = neurone.Perceptron(kernel="rbf_ker", sigma=5.12)
+
+clf_linear.fit(X_train, y_train)
+clf_poly.fit(X_train, y_train)
+clf_rbf.fit(X_train, y_train)
+
+# print("Deve uscire: " + str(y_test[0]) + "ma esce: " + str(clf.predict(X_test[0])))
+
+accuracy_linear = clf_linear.predict_set(X_test, y_test)
+accuracy_poly = clf_poly.predict_set(X_test, y_test)
+accuracy_rbf = clf_rbf.predict_set(X_test, y_test)
+
+print("Kernel Lineare: " + str(accuracy_linear))
+print("Kernel Poly: " + str(accuracy_poly))
+print("Kernel RBF: " + str(accuracy_rbf))
